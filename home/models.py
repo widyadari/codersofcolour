c5fd3a98610f6c93995bcf53e158dfb33b6a7f45
Anastasia Widyadari <widi.adari@hotmail.com>
2020-06-09 20:31:39 +0100

    Heroku configuration

--- home/models.py
+++ home/models.py
@@ -2,7 +2,6 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
-
 class HomePage(Page):
     body = RichTextField(blank=True)
     content_panels = Page.content_panels + [
