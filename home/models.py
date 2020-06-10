c1eb3eb93ed3b7d7992cec6d9b0e0ce476d57bd8
Anastasia Widyadari <widi.adari@hotmail.com>
2020-06-10 18:05:12 +0100

    Project structure needed restructuring

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+
 class HomePage(Page):
     body = RichTextField(blank=True)
     content_panels = Page.content_panels + [
